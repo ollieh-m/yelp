c6afb6963a1427efbad4eeb3250be2b56a60f36a
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-05-24 13:03:58 +0200

    Section 10: Updating

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -32,7 +32,6 @@ feature 'restaurants' do
     end
   end
 
-
   context 'viewing restaurants' do
 
     let!(:kfc){ Restaurant.create(name:'KFC') }
@@ -43,6 +42,22 @@ feature 'restaurants' do
       expect(page).to have_content 'KFC'
       expect(current_path).to eq "/restaurants/#{kfc.id}"
     end
-
   end
+
+  context 'editing restaurants' do
+
+    before { Restaurant.create name: 'KFC' }
+
+    scenario 'let a user edit a restaurant' do
+      visit '/restaurants'
+      click_link 'Edit KFC'
+      fill_in 'Name', with: 'Kentucky Fried Chicken'
+      fill_in 'Description', with: 'Deep fried goodness'
+      click_button 'Update Restaurant'
+      expect(page).to have_content 'Kentucky Fried Chicken'
+      expect(page).to have_content 'Deep fried goodness'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
+
 end
