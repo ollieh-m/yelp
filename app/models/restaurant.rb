70d98dc174c6187c19dce51ee0d10bc6985e648b
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-05-25 18:05:12 +0100

    yelp v3 section 3 complete

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,13 +1,17 @@
 class Restaurant < ActiveRecord::Base
   belongs_to :user
-  has_many :reviews, dependent: :destroy
+  # has_many :reviews, dependent: :destroy
   validates :name, length: { minimum: 3 }, uniqueness: true
 
-  has_many :reviews do
-    def build_with_user(attributes = {}, user)
-      attributes[:user] ||= user
-      build(attributes)
-    end
-  end
+  has_many :reviews,
+      -> { extending WithUserAssociationExtension },
+      dependent: :destroy
+
+  # has_many :reviews do
+  #   def build_with_user(attributes = {}, user)
+  #     attributes[:user] ||= user
+  #     build(attributes)
+  #   end
+  # end
 
 end
