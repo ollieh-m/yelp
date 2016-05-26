ba92026c0600aadf6ca3910fa5bfeb87fec41e7f
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-05-26 22:53:38 +0100

    partials

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,18 +7,21 @@ class Restaurant < ActiveRecord::Base
       -> { extending WithUserAssociationExtension },
       dependent: :destroy
 
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
   def average_rating
     return "N/A" if reviews.none?
     reviews.average(:rating)
-    # sum = 0
-    # i = 0
-    # reviews.each do |review|
-    #   sum += review.rating
-    #   i += 1
-    # end
-    # # p reviews
-    # # reviews.average(:rating)
-    # return sum/i.to_f
+    sum = 0
+    i = 0
+    reviews.each do |review|
+      sum += review.rating
+      i += 1
+    end
+    # p reviews
+    # reviews.average(:rating)
+    return sum/i.to_f
 
     # p "average: #{reviews.average(:rating)}"
     # return sum/i.to_f
