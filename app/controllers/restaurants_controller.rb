783bb5a0f66d628c8ade232bfac4e1d55091a88a
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-05-26 17:00:57 +0200

    5: Adding images part 1

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -19,7 +19,7 @@ class RestaurantsController < ApplicationController
   end
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :description)
+    params.require(:restaurant).permit(:name, :description, :image)
   end
 
   def show
