810aeebd29af0c20b17ecbb9a87bb66df63a6f9e
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-05-26 22:31:56 +0100

    commit replicating error in unit test

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -9,15 +9,16 @@ class Restaurant < ActiveRecord::Base
 
   def average_rating
     return "N/A" if reviews.none?
-    sum = 0
-    i = 0
-    reviews.each do |review|
-      sum += review.rating
-      i += 1
-    end
-    # p reviews
-    # reviews.average(:rating)
-    return sum/i.to_f
+    reviews.average(:rating)
+    # sum = 0
+    # i = 0
+    # reviews.each do |review|
+    #   sum += review.rating
+    #   i += 1
+    # end
+    # # p reviews
+    # # reviews.average(:rating)
+    # return sum/i.to_f
 
     # p "average: #{reviews.average(:rating)}"
     # return sum/i.to_f
