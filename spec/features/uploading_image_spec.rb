af9ef4a24c6bed14e9f5dfb9698c500b30c02c4d
ollieh-m <ollie.hm@cantab.net>
2016-05-27 10:51:56 +0100

    Modify image upload test

--- spec/features/uploading_image_spec.rb
+++ spec/features/uploading_image_spec.rb
@@ -12,14 +12,14 @@ feature 'uploading images' do
   end
 
 	scenario 'user can upload and view an image of a restaurant' do
-		click_link 'Add a restaurant'
+    click_link 'Add a restaurant'
     fill_in 'Name', with: 'AbraKadabra'
     attach_file('Image', Rails.root + "spec/fixtures/ABRAKABABRA.jpeg")
     click_button 'Create Restaurant'
-		expect(page).to have_css("img[src*='ABRAKABABRA.jpeg']")
+    expect(page).to have_css("img[src*='ABRAKABABRA.jpeg']")
     expect(current_path).to eq '/restaurants'
     click_link 'AbraKadabra'
-		expect(page).to have_css("img[src*='ABRAKABABRA.jpeg']")
+    expect(page).to have_css("img[src*='ABRAKABABRA.jpeg']")
 	end
 
 end
